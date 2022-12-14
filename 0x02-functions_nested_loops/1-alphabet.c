5abd934d597ce5056a03791f18fb18c9a36363f1
CaptainTeezy <obisesantolulopejohnson@gmail.com>
2022-12-14 23:16:17 +0100

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,16 @@
 /**
  *print_alphabet(void)-function
  *write a function that prints alphabets in lowercase
- *Return: Always 0 (success)
+ * 
 */
 void print_alphabet(void)
 {
 	int i = 'a';
 
 	for (; i <= 'z'; i++)
-		_putchar(i);
+	{	_putchar(i);
+
+	}
 		_putchar('\n');
 
-		return (0);
 }
