6133de1eb7dcb643787a7af6aceb42aefa8b7109
CaptainTeezy <obisesantolulopejohnson@gmail.com>
2022-12-15 12:15:40 +0100

    tasked 5

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,15 +3,13 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
- * Return: On success 1.
+ * * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-<<<<<<< HEAD
 	return (write(1, &c, 1));
-=======
-        return (write(1, &c, 1));
->>>>>>> c45b54b19ac9419cab217e41b075cef793f3f2cd
+
+	return (write(1, &c, 1));
+
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,9 +5,7 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-<<<<<<< HEAD
 int print_sign(int n);
-=======
 int _isalpha(int c);
->>>>>>> c45b54b19ac9419cab217e41b075cef793f3f2cd
+int _abs(int);
 #endif
