eb99397ce4b24dd26d8f872b868b8ab2e2eebeec
CaptainTeezy <obisesantolulopejohnson@gmail.com>
2023-01-03 15:07:15 +0100

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * mul - check description
+ * Description: multiplies two integers
+ * Return: Always 0
+ * @a: input
+ * @b: input
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
